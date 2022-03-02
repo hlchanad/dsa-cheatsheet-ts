a4e1ea2a2518a98a75199e1b4e62a8953b583d1d
hlchanad <hlchanad@chanhonlun.com>
2022-03-02 12:22:13 +0800

    merge sort

--- src/__test__/sort.spec.ts
+++ src/__test__/sort.spec.ts
@@ -1,8 +1,15 @@
-import { quickSort } from "../sort";
+import { mergeSort, quickSort } from "../sort";
 
 describe('Quick Sort', () => {
     it('sorts array', () => {
         const array = [6,5,4,1,2,2,7,8,3];
         expect(quickSort([...array])).toEqual(array.sort());
+    });
+});
+
+describe('Merge Sort', () => {
+    it('sorts array', () => {
+        const array = [6,5,4,1,2,2,7,8,3];
+        expect(mergeSort([...array])).toEqual(array.sort());
     });
 });
--- src/sort.ts
+++ src/sort.ts
@@ -44,3 +44,49 @@ export function quickSort(array: number[]): number[] {
     _quickSort(0, array.length - 1);
     return array;
 }
+
+/**
+ * space complexity: O() ????
+ * time complexity: O(n log n)
+ * @param array
+ */
+export function mergeSort(array: number[]): number[] {
+    const merge = (...arrays: number[][]) => {
+        const indices = new Array(arrays.length).fill(0);
+        const isFinished = () =>
+            arrays.every((array, index) => array.length === indices[index]);
+
+        const result: number[] = [];
+
+        while (!isFinished()) {
+            let smallest, smallestIndex;
+            arrays.forEach((array, index) => {
+                if (array.length <= 0) return;
+
+                const value = array[indices[index]]
+
+                if (smallest === undefined || value < smallest) {
+                    smallest = value;
+                    smallestIndex = index;
+                }
+            });
+
+            result.push(smallest);
+            indices[smallestIndex]++;
+        }
+
+        return result;
+    }
+
+    const _mergeSort = (array: number[]): number[] => {
+        if (array.length <= 1) return array;
+
+        const middle = Math.floor(array.length / 2);
+        const left = array.slice(0, middle);
+        const right = array.slice(middle, array.length);
+
+        return merge(_mergeSort(left), _mergeSort(right));
+    };
+
+    return _mergeSort(array);
+}
